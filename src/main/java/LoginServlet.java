32a7a5596d50e2da540b3693b81f75dc8d8c3972
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-01-24 00:48:52 -0600

    fixed profile page

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -12,7 +12,7 @@ import javax.servlet.ServletException;
         import javax.servlet.http.HttpServletResponse;
         import java.io.IOException;
 
-@WebServlet(name = "ViewProfileServlet", urlPatterns = "/login")
+@WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
@@ -26,7 +26,7 @@ public class LoginServlet extends HttpServlet {
             String username = request.getParameter("username");
             String password = request.getParameter("password");
             if (username.equals("admin") && password.equals("password")) {
-                response.sendRedirect("/profile");
+                response.sendRedirect("/profile.jsp");
             }else{
                 response.sendRedirect("/login?error=try-again");
             }
