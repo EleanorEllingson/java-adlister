2a9e602792932c774d4d3c84bce10c23599c0908
Eleanor Ellingson <eleanor.ellingson@gmail.com>
2021-01-26 17:38:21 -0600

    Hardcoded ad and checked functionality

--- src/main/java/Config.java
+++ src/main/java/Config.java
@@ -4,10 +4,10 @@ public class Config {
         return "jdbc:mysql://localhost/adlister_db?serverTimezone=UTC&useSSL=false";
     }
     public String getUser() {
-        return "someuser";
+        return "adListerUser";
     }
     public String getPassword() {
-        return "strongpassword";
+        return "adlisterpw";
     }
 
 
